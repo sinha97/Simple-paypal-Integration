624d0db33f12b4a230f8f0764a4af444e80c6350
sinha97 <vk9633698@gmail.com>
2021-02-12 10:36:07 +0530

    initial payment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
+import ReactDOM from "react-dom"
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
 function App() {
+
+  const [price, setPrice] = useState(0)
+
+  const createOrder = (data, actions) =>{
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: price,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    console.log(data);
+    return actions.order.capture();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <input type="number" onChange={e=>setPrice(e.target.value)} value={price} />
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
     </div>
   );
 }
